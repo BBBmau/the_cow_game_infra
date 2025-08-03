d8bdad37a974abe03350e9c1d6166a35a99d923d
Mauricio Alvarez Leon <alvarez.mauriciotm@gmail.com>
2025-08-02 18:47:07 -0700

    update machine type to e2-stand-2

--- GKE.tf
+++ GKE.tf
@@ -64,7 +64,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
 
   node_config {
     preemptible  = true
-    machine_type = "e2-medium"
+    machine_type = "e2-standard-2"
 
     tags = ["game-server"]
 
