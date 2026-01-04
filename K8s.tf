490a22d3fbb353f65f27728d27b2100a67729955
Mauricio Alvarez Leon <alvarez.mauriciotm@gmail.com>
2026-01-03 23:15:39 -0800

    use KinD insteaad of minikube

--- K8s.tf
+++ K8s.tf
@@ -1,6 +1,54 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "tehcyx/kind"
+      version = "0.4.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "kind" {}
+
+resource "kind_cluster" "cow_game" {
+  name = "cow-game-prod"
+  # Multi-node for HA
+  kind_config {
+    kind = "Cluster"
+    api_version = "kind.x-k8s.io/v1alpha4"
+    node {
+        role = "control-plane"
+        extra_port_mappings {
+          container_port = 30080
+          host_port      = 30080
+          protocol       = "TCP"
+        }
+    }
+    }
+}
+
+# Automatically load the local Docker image into the kind cluster
+resource "null_resource" "load_image" {
+  depends_on = [kind_cluster.cow_game]
+
+  triggers = {
+    cluster_name = kind_cluster.cow_game.name
+    # Re-run if cluster is recreated
+    cluster_id = kind_cluster.cow_game.id
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      docker save mmo-server:local | docker exec -i ${kind_cluster.cow_game.name}-control-plane ctr -n=k8s.io images import -
+    EOT
+  }
+}
+
 provider "kubernetes" {
-  host                   = "https://127.0.0.1:64145"
-  cluster_ca_certificate = file("/Users/mau/.minikube/ca.crt")
+  host                   = kind_cluster.cow_game.endpoint
+  cluster_ca_certificate = kind_cluster.cow_game.cluster_ca_certificate
 }
 
 // we moved away from pods in order to have auto provisioning every day - perhaps we can prevent this from happening
@@ -79,7 +127,8 @@ resource "kubernetes_deployment" "game_server" {
   }
 
   depends_on = [
-    kubernetes_service.redis
+    kubernetes_service.redis,
+    null_resource.load_image
   ]
 }
 
@@ -146,6 +195,7 @@ resource "kubernetes_service" "the_cow_game_server" {
     port {
       port        = 80
       target_port = 6060
+      node_port   = 30080
     }
   }
 }
