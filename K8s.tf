7d8f1ae856a1cd95910b39b872a45568f95f3856
Mauricio Alvarez Leon <alvarez.mauriciotm@gmail.com>
2026-01-03 20:00:12 -0800

    local minikube setup

--- K8s.tf
+++ K8s.tf
@@ -195,6 +195,25 @@ resource "kubernetes_service" "redis" {
   }
 }
 
+resource "kubernetes_service" "the_cow_game_server" {
+  metadata {
+    name = "the-cow-game-server"
+  }
+
+  spec {
+    selector = {
+      app = "cow-game"
+    }
+
+    type = "NodePort"
+
+    port {
+      port        = 80
+      target_port = 6060
+    }
+  }
+}
+
 // Persistent Volume Claim for Redis
 resource "kubernetes_persistent_volume_claim" "redis" {
   metadata {
@@ -211,130 +230,3 @@ resource "kubernetes_persistent_volume_claim" "redis" {
     }
   }
 }
-
-# resource "kubernetes_secret" "artifact_registry_secret" {
-#   metadata {
-#     name = "artifact-registry-secret"
-#   }
-
-#   type = "kubernetes.io/dockerconfigjson"
-
-#   data = {
-#     ".dockerconfigjson" = jsonencode({
-#       auths = {
-#         "us-west1-docker.pkg.dev" = {
-#           username = "_json_key"
-#           password = file("../../../credentials/thecowgame-clustermanager.json")
-#           email    = "cluster-manager@thecowgame.iam.gserviceaccount.com"
-#           auth     = base64encode(format("%s:%s", "_json_key", file("../../../credentials/thecowgame-clustermanager.json")))
-#         }
-#       }
-#     })
-#   }
-# }
-
-
-# resource "kubernetes_service" "headless_service" {
-#   metadata{
-#     name = "single-pod-service"
-#   }
-  
-#   spec {
-#     type = "NodePort"   # Headless service disables load balancing
-#     selector = {
-#       "app" = "cow-game"
-#     }
-#     port{
-#       port = 80
-#       target_port = 6060
-#     }
-#   }
-# }
-
-# resource "google_compute_backend_service" "ingress_backend" {
-#   name        = "k8s-be-31622--109cb19e972eba76"
-#   protocol    = "HTTP"
-#   timeout_sec = 3600
-
-#   connection_draining_timeout_sec = 300  # replaces connection_draining block
-
-#   health_checks = [
-#     "https://www.googleapis.com/compute/v1/projects/thecowgame/global/healthChecks/k8s1-109cb19e-kube-system-default-http-backend-80-cb1c11b2"
-#   ]
-
-#   backend {
-#     group = "https://www.googleapis.com/compute/v1/projects/thecowgame/zones/us-west1-a/instanceGroups/k8s-ig--109cb19e972eba76"
-#   }
-
-#   lifecycle {
-#     ignore_changes = [
-#       health_checks,
-#       description
-#     ]
-#   }
-# }
-
-
-# resource "kubernetes_ingress_v1" "gke_ingress" {
-#   metadata {
-#     name = "playhtecowgame-ingress"
-#     annotations = {
-#       "kubernetes.io/ingress.global-static-ip-name" = google_compute_global_address.default.name
-#       "networking.gke.io/managed-certificates"       = kubernetes_manifest.managed_certificate.manifest["metadata"]["name"]
-#     }
-#   }
-
-#   spec {
-#     rule {
-#       http {
-#         path {
-#           path     = "/*"
-#           path_type = "ImplementationSpecific"
-#           backend {
-#             service {
-#               name = kubernetes_service.headless_service.metadata[0].name
-#               port {
-#                 number = 80
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
-
-#   depends_on = [kubernetes_manifest.managed_certificate]
-# }
-
-# Create the ManagedCertificate Kubernetes resource
-# resource "kubernetes_manifest" "managed_certificate" {
-#   manifest = {
-#     apiVersion = "networking.gke.io/v1beta1"
-#     kind       = "ManagedCertificate"
-#     metadata = {
-#       name      = "playthecowgame-cert"
-#       namespace = "default"
-#     }
-#     spec = {
-#       domains = [
-#         "www.playthecowgame.com"
-#       ]
-#     }
-#   }
-# }
-
-# resource "kubernetes_manifest" "managed_certificate_2" {
-#   manifest = {
-#     apiVersion = "networking.gke.io/v1beta1"
-#     kind       = "ManagedCertificate"
-#     metadata = {
-#       name      = "playthecowgame-cert-2"
-#       namespace = "default"
-#     }
-#     spec = {
-#       domains = [
-#         "playthecowgame.com"
-#       ]
-#     }
-#   }
-# }
